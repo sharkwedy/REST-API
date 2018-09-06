86fd04225628f15e80f196c5bc95b439b8d192f5
sharkwedy <talls.urin@hotmail.com>
2018-09-06 00:46:08 -0300

    v

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://thalesmelo:Heroku1*@ds147592.mlab.com:47592/heroku_m5bjc2zk';
+const URI = 'mongodb://heroku_m5bjc2zk:Heroku1*@ds147592.mlab.com:47592/heroku_m5bjc2zk';
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
--- routes/test.routes.js
+++ routes/test.routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 
-const movie = require('../controllers/test.controller');
-
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
   res.send("Requisição GET");
