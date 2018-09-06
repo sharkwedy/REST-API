578d7207580e6d24ab226aafb076845b0896abdc
sharkwedy <talls.urin@hotmail.com>
2018-09-05 23:22:10 -0300

    foi

--- models/evaluation.js
+++ models/evaluation.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const evaluationSchema = new Schema({
+const EvaluationSchema = new Schema({
     name: { type: String, required: true },
     movie: { type: String, required: true },
     evaluation: { type: String, required: true }
