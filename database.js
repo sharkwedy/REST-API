3da0d9bbf2d396dd456d7b7318a464d5ed4a2f43
sharkwedy <talls.urin@hotmail.com>
2018-09-06 00:52:29 -0300

    t

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://heroku_m5bjc2zk:Heroku1*@ds147592.mlab.com:47592/heroku_m5bjc2zk';
+const URI = 'mongodb://User:Heroku1*@ds147592.mlab.com:47592/heroku_m5bjc2zk';
 
 mongoose.connect(URI)
     .then(db => console.log('DB is connected'))
