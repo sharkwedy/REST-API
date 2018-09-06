b5a76a7f37bb1eee91ca3a5103826c39e52ccb0c
sharkwedy <talls.urin@hotmail.com>
2018-09-05 21:42:15 -0300

    restapi

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { mongoose } = require('./database');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+
+// Routes
+app.use('/api/movies', require('./routes/movie.routes'));
+app.use('/api/movies', require('./routes/evaluation.routes'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
